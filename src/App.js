87daee00a4ed25b8db198d17e9f18cf6c6e9d6f1
Amdrii <andreyk557@gmail.com>
2023-03-22 20:10:05 +0200

    feat[found-pair]

--- src/App.js
+++ src/App.js
@@ -27,22 +27,24 @@ function App() {
   return (
     <div className="App">
       <div className="board">
-        {cards.map((card, index) => (
-          <div
-            className={
-              "card-outer " +
-              (activeCards.indexOf(index) !== -1 ? "flipped" : "")
-            }
-            onClick={() => flipCard(index)}
-          >
-            <div className="card">
-              <div className="front">
-                <img src={card}></img>
+        {cards.map((card, index) => {
+          const flippedToFront =
+            activeCards.indexOf(index) !== -1 ||
+            foundPairs.indexOf(index) !== -1;
+          return (
+            <div
+              className={"card-outer " + (flippedToFront ? "flipped" : "")}
+              onClick={() => flipCard(index)}
+            >
+              <div className="card">
+                <div className="front">
+                  <img src={card}></img>
+                </div>
+                <div className="back"></div>
               </div>
-              <div className="back"></div>
             </div>
-          </div>
-        ))}
+          );
+        })}
       </div>
     </div>
   );
