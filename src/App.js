aad7dd16faf9de1a87351f1b22b1ba305751c3a9
Amdrii <andreyk557@gmail.com>
2023-03-22 19:48:59 +0200

    feat[flepped-animation /]

--- src/App.js
+++ src/App.js
@@ -5,12 +5,22 @@ import { shuffle } from "lodash";
 
 function App() {
   const [cards, setCards] = useState(shuffle([...Images, ...Images]));
+  const [activeCards, setActiveCards] = useState([]);
+  function flipCard(index) {
+    setActiveCards([index]);
+  }
 
   return (
     <div className="App">
       <div className="board">
         {cards.map((card, index) => (
-          <div className="card-outer">
+          <div
+            className={
+              "card-outer " +
+              (activeCards.indexOf(index) !== -1 ? "flipped" : "")
+            }
+            onClick={() => flipCard(index)}
+          >
             <div className="card">
               <div className="front">
                 <img src={card}></img>
